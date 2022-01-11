2d37c7f407841b7d424ff24dce5421a4ef383a74
Brunoamt <brunoamt2_1502@hotmil.com>
2022-01-11 16:18:27 -0300

    requisitos 1 2 3 feitos

---
+++ script.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('.trybewarts-login');
+const login = document.querySelector('#email');
+const password = document.querySelector('#password');
+const btnEntrar = document.querySelector('#btn-entrar');
+
+function emailAlert() {
+  if (login.value === 'tryber@teste.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+
+
+
+
+
+
+
+function callEvent() {
+  btnEntrar.addEventListener('click', emailAlert);
+}
+callEvent();
